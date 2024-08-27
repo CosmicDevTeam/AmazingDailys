5b80ff00ebadfd121dd94e208fb1784f33d1201e
Zephyrhz0 <162658447+CosmicDevTeam@users.noreply.github.com>
2024-08-27 14:48:12 -0700

    Actualizar daily

---
+++ src/zephy/daily/cooldown/Cooldown.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace zephy\daily\cooldown;
+
+final class Cooldown
+{
+
+    private array $cooldowns = [];
+
+    public function getCooldowns(): array
+    {
+        return $this->cooldowns;
+    }
+
+    public function inCooldown(string $player): bool
+    {
+        if (isset($this->cooldowns[$player])) {
+            return $this->cooldowns[$player] > time();
+        }
+        return false;
+    }
+
+    public function getCooldown(string $player): int
+    {
+        return $this->cooldowns[$player] - time();
+    }
+
+    public function addCooldown(string $player, int $time): void
+    {
+        $this->cooldowns[$player] = time() + $time;
+    }
+
+    public function removeCooldown(string $player): void
+    {
+        $this->cooldowns[$player] = 0;
+    }
+}
