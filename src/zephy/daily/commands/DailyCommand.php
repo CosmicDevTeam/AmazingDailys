f63c8d9a6e60243d227ca5c221395008e8176e3f
Zephyrhz0 <162658447+CosmicDevTeam@users.noreply.github.com>
2024-08-27 14:48:57 -0700

    Actualizar daily

---
+++ src/zephy/daily/commands/DailyCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace zephy\daily\commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use zephy\daily\forms\admin\CreatorForm;
+use zephy\daily\forms\admin\DeleteForm;
+use zephy\daily\forms\admin\ItemsMenu;
+use zephy\daily\forms\DailyMenu;
+use zephy\daily\manager\DailyFactory;
+use zephy\daily\utils\PermissionUtils;
+use zephy\daily\utils\TextUtils;
+
+class DailyCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct("daily", "Open daily rewards", null, ["rewards"]);
+        $this->setPermission(PermissionUtils::DEFAULT);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if (!$sender instanceof Player) {
+            return;
+        }
+
+        if (!isset($args[0])) {
+            DailyMenu::send($sender);
+            return;
+        }
+
+        switch ($args[0]) {
+            case "create":
+                if ($sender->hasPermission(PermissionUtils::ADMIN)) {
+                    $sender->sendForm(new CreatorForm());
+                    return;
+                }
+                break;
+            case "delete":
+                if ($sender->hasPermission(PermissionUtils::ADMIN)) {
+                    $sender->sendForm(new DeleteForm());
+                    return;
+                }
+                break;
+            case "setitems":
+            case "rewards":
+                if ($sender->hasPermission(PermissionUtils::ADMIN)) {
+                    if (!isset($args[1])) {
+                        $sender->sendMessage(TextUtils::formatMessage(TextUtils::getMessages()->get("error-items-arguments")));
+                        return;
+                    }
+
+                    if (is_null(DailyFactory::getInstance()->getDaily($args[1]))) {
+                        $sender->sendMessage(TextUtils::formatMessage(TextUtils::getMessages()->get("daily-not-exists"), [
+                            "{DAILY}" => $args[1]
+                        ]));
+                        return;
+                    }
+
+                    $daily = DailyFactory::getInstance()->getDaily($args[1]);
+                    ItemsMenu::send($sender, $daily);
+                    break;
+                }
+        }
+    }
+}
